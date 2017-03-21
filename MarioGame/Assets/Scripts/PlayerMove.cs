c01180ef18d6690851d18cd075bec5ad6e1c6f48
Mirel Aioanei <themyrel@yahoo.com>
2017-03-21 19:55:09 +0200

    Import textures
    Create basic prefabs,add demo player and script for movement

---
+++ MarioGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    public class PlayerMove : MonoBehaviour
+    {
+        public float Speed = 1.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+
+        void Update()
+        {
+            var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            this.transform.position += move * Speed * Time.deltaTime;
+        }
+    }
+}
