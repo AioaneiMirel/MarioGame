dd9f46bb39d8f9b5dd49aa6acfdc46eb6d2f62ba
Mirel Aioanei <themyrel@yahoo.com>
2017-03-22 20:46:27 +0200

    CoinSpin
    Start creating Coin object

---
+++ MarioGame/Assets/Scripts/CoinSpin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CoinSpin : MonoBehaviour
+    {
+        public int Speed = 1;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(0, 5 * Speed, 0, Space.World);
+        }
+    }
+}
--- MarioGame/Assets/Scripts/PlayerMove.cs
+++ MarioGame/Assets/Scripts/PlayerMove.cs
@@ -4,7 +4,7 @@
 
     public class PlayerMove : MonoBehaviour
     {
-        public float Speed = 1.0f;
+        public float Speed = 3.0f;
 
         // Use this for initialization
         void Start()
