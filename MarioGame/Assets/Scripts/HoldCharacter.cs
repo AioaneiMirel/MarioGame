72e1c163bd6388ddce129b4c1a5f552c9810ec35
Mirel Aioanei <themyrel@yahoo.com>
2017-03-29 18:00:57 +0300

    Platforms
    Adding moving and jumping platforms

---
+++ MarioGame/Assets/Scripts/HoldCharacter.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    public class HoldCharacter : MonoBehaviour
+    {
+        // Use this for initialization
+        void OnTriggerEnter(Collider col)
+        {
+            col.transform.parent = gameObject.transform; //when in collidion with object make thar object it's parent(player does not fall from platform)
+        }
+
+        void OnTriggerExit(Collider col)
+        {
+            col.transform.parent = null;
+        }
+    }
+}
